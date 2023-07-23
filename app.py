e8396c3bc76c7e304f860afe211e568a49f35508
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-07-23 20:30:17 +0200

    Add web app script

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Hello world")
+
+st.write("Here's our first attempt at using data to create a table : ")
+
+#st.write(pd.DataFrame())
+
+st.text("This is some text. ")
